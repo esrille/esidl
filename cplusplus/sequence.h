0babcd0c828197243a3ee282d9e220e83f025486
Shiki Okasaka <shiki.okasaka@gmail.com>
2010-02-26 10:54:25 +0000

    (Sequence) : Revise to provide the compatible operations with ObjectArray.
    git-svn-id: http://es-operating-system.googlecode.com/svn/trunk/esidl@1471 d15c3b8f-5b43-0410-be93-4d5a55ce2954

--- cplusplus/sequence.h
+++ cplusplus/sequence.h
@@ -17,100 +17,233 @@
 #ifndef ES_SEQUENCE_INCLUDED
 #define ES_SEQUENCE_INCLUDED
 
+#include <org/w3c/dom/ObjectArray.h>
 #include <new>
 
 // The sequence type for Web IDL
-// TODO: How could we improve the implementation for providing better performance?
+// TODO: This implementation is not multi-thread safe.
 template <typename T>
 class Sequence
 {
-    T* sequence;
-    size_t length;
-
-    void init(size_t size)
+    class Rep : public org::w3c::dom::ObjectArray<T>
     {
-        length = size;
-        sequence = (0 < length) ? new T[length] : 0;
-    }
+        friend class Sequence;
+        // stub
+        T* sequence;
+        unsigned int length;
+        // proxy
+        org::w3c::dom::ObjectArray<T>* array;
+        // reference count
+        unsigned count;
 
-    void clone(const T* array, size_t size)
-    {
-        init(size);
-        for (size_t i = 0; i < length; ++i)
+        void init(unsigned int size)
         {
-            sequence[i] = array[i];
+            array = 0;
+            length = size;
+            sequence = (0 < length) ? new T[length] : 0;
+            count = 1;
         }
-    }
+
+        void copy(const T* array, unsigned int size)
+        {
+            for (unsigned int i = 0; i < length; ++i)
+            {
+                sequence[i] = array[i];
+            }
+        }
+
+        // Prevent copying
+        Rep(const Rep& value);
+        Rep& operator=(const Rep& value);
+
+    public:
+        Rep()
+        {
+            init(0);
+        }
+
+        Rep(const T* sequence, unsigned int length)
+        {
+            init(length);
+            copy(sequence, length);
+        }
+
+        Rep(unsigned int size)
+        {
+            init(size);
+        }
+
+        Rep(org::w3c::dom::ObjectArray<T>* array) 
+        {
+            init(0);
+            this->array = array;
+        }
+
+        ~Rep()
+        {
+            if (sequence)
+            {
+                delete[] sequence;
+            }
+            delete array;
+        }
+
+        virtual T getElement(unsigned int index)
+        {
+            if (array)
+            {
+                return array->getElement(index);
+            }
+            return sequence[index];
+        }
+        
+        virtual void setElement(unsigned int index, const T value)
+        {
+            if (array)
+            {
+                array->setElement(index, value);
+                return;
+            }
+            sequence[index] = value;
+        }
+        
+        virtual unsigned int getLength()
+        {
+            if (array)
+            {
+                return array->getLength();
+            }
+            return length;
+        }
+        
+        virtual void setLength(unsigned int length)
+        {
+            if (array)
+            {
+                array->setLength(length);
+                return;
+            }
+            // TODO: Raise an exception
+        }
+    };
+
+    Rep* rep;
 
 public:
-    Sequence() :
-        sequence(0),
-        length(0)
+    class Ref
     {
+        friend class Sequence;
+        Sequence& sequence;
+        unsigned int index;
+
+        Ref(Sequence& sequence, unsigned int index) :
+            sequence(sequence),
+            index(index)
+        {
+        }
+    public:
+        operator T()
+        {
+            return sequence.getElement(index);
+        }
+        void operator=(T value)
+        {
+            sequence.setElement(index, value);
+        }
+    };
+
+    Sequence()
+    {
+        rep = new Rep();
     }
 
-    Sequence(const T* array, size_t size)
+    Sequence(const T* array, unsigned int size)
     {
-        clone(array, size);
+        rep = new Rep(array, size);
     }
 
-    Sequence(size_t size)
+    Sequence(unsigned int size)
     {
-        init(size);
+        rep = new Rep(size);
+    }
+    
+    Sequence(org::w3c::dom::ObjectArray<T>* array)
+    {
+        rep = new Rep(array);
     }
 
-    /** Copy-constructor.
-     */
+    // Copy-constructor
     Sequence(const Sequence& value)
     {
-        clone(value.sequence, value.length);
+        ++value.rep->count;
+        rep = value.rep;
     }
 
     ~Sequence()
     {
-        if (sequence)
+        if (--rep->count == 0)
         {
-            delete[] sequence;
+            delete rep;
         }
     }
 
     Sequence& operator=(const Sequence& value)
     {
-        clone(value.sequence, value.size);
+        ++value.rep->count;
+        if (--rep->count == 0)
+        {
+            delete rep;
+        }
+        rep = value.rep;
         return *this;
+    }
+
+    T getElement(unsigned int index)
+    {
+        return rep->getElement(index);
+    }
+
+    void setElement(unsigned int index, const T value)
+    {
+        rep->setElement(index, value);
     }
 
     T operator[](int index) const
     {
-        return sequence[index];
+        return rep->getElement(index);
     }
 
-    T& operator[](int index)
+    Ref operator[](int index)
     {
-        return sequence[index];
+        return Ref(*this, index);
     }
 
-    T at(size_t index) const
+    T at(unsigned int index) const
     {
-        if (length <= index)
+        if (getLength() <= index)
         {
             // TODO: raise an exception;
         }
-        return sequence[index];
+        return rep->getElement(index);
     }
 
-    T& at(size_t index)
+    Ref at(unsigned int index)
     {
-        if (length <= index)
+        if (getLength() <= index)
         {
             // TODO: raise an exception;
         }
-        return sequence[index];
+        return Ref(*this, index);
     }
 
-    size_t getLength() const
+    unsigned int getLength() const
     {
-        return length;
+        return rep->getLength();
+    }
+
+    operator org::w3c::dom::ObjectArray<T>*()
+    {
+        return rep;
     }
 };
 
