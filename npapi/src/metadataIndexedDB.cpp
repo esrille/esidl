6f5d2c7fdf8a0c89a97211b4d8acd42373055d41
Shiki Okasaka <shiki.okasaka@gmail.com>
2010-04-15 08:37:27 +0000

    (Indexed Database) : Update to Editor's Draft 15 April 2010.
    git-svn-id: http://es-operating-system.googlecode.com/svn/trunk/esidl@1608 d15c3b8f-5b43-0410-be93-4d5a55ce2954

--- npapi/src/metadataIndexedDB.cpp
+++ npapi/src/metadataIndexedDB.cpp
@@ -23,26 +23,26 @@ using namespace org::w3c::dom;
 
 void initializeIndexedDBMetaData()
 {
-    ProxyControl::registerMetaData(indexeddb::Cursor::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::Cursor_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::CursorRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::CursorRequest_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::CursorSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::CursorSync_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::DatabaseError::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::DatabaseError_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::Database::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::Database_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::DatabaseRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::DatabaseRequest_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::DatabaseSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::DatabaseSync_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::DBRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::DBRequest_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::Environment::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::Environment_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::EnvironmentSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::EnvironmentSync_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBCursor::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBCursor_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBCursorRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBCursorRequest_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBCursorSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBCursorSync_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBDatabaseError::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBDatabaseError_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBDatabase::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBDatabase_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBDatabaseRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBDatabaseRequest_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBDatabaseSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBDatabaseSync_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBEnvironment::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBEnvironment_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBEnvironmentSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBEnvironmentSync_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBIndex::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBIndex_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBIndexRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBIndexRequest_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBIndexSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBIndexSync_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBKeyRange::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBKeyRange_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBObjectStore::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBObjectStore_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBObjectStoreRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBObjectStoreRequest_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBObjectStoreSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBObjectStoreSync_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBRequest_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBTransaction::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBTransaction_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBTransactionRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBTransactionRequest_Bridge<Any, invoke> >::createInstance);
+    ProxyControl::registerMetaData(indexeddb::IDBTransactionSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IDBTransactionSync_Bridge<Any, invoke> >::createInstance);
     ProxyControl::registerMetaData(indexeddb::IndexedDatabase::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IndexedDatabase_Bridge<Any, invoke> >::createInstance);
     ProxyControl::registerMetaData(indexeddb::IndexedDatabaseRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IndexedDatabaseRequest_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::Index::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::Index_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::IndexRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IndexRequest_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::IndexSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::IndexSync_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::KeyRange::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::KeyRange_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::ObjectStore::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::ObjectStore_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::ObjectStoreRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::ObjectStoreRequest_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::ObjectStoreSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::ObjectStoreSync_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::Transaction::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::Transaction_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::TransactionRequest::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::TransactionRequest_Bridge<Any, invoke> >::createInstance);
-    ProxyControl::registerMetaData(indexeddb::TransactionSync::getMetaData(), Proxy_Impl<ProxyObject, indexeddb::TransactionSync_Bridge<Any, invoke> >::createInstance);
 }
